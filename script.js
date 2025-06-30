a0c14c190513a0a95f5d3d30853a3992143e0971
adityakumar107 <adityakumar2882003@gmail.com>
2025-06-30 18:56:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// hamko karna ye hai ki jab uspe click kare to jo 1 likha hua hai wo badhe
+// jo cart ke upar icon ke pass likhe hua hai wo bhi badhe
+// or jab add to cart kare to waha par 1 item add ho jaye
+
+// var btn = document.querySelector("#btn")
+// var increaseNum = document.querySelector(".box")
+// var plusMinus = document.querySelector(".Plus-minus")
+// var increase = document.querySelector(".plus")
+// var decrease = document.querySelector(".minus")
+// btn.addEventListener("click",()=>{
+// increaseNum.innerHTML="1",
+// increaseNum.style.opacity=1
+// plusMinus.innerHTML=1
+// })
+// increase.addEventListener("click",()=>{
+//     increaseNum.innerHTML="1",
+//     increaseNum.style.opacity=1
+//     plusMinus.innerHTML=1
+// })
+// decrease.addEventListener("click",()=>{
+//     increaseNum.innerHTML="0",
+//     increaseNum.style.opacity=0
+//     plusMinus.innerHTML=0
+// })
+for(var i=1; i<11; i++){
+    
+}
+console.log(i)
